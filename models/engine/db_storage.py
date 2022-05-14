f7a00a2d7aa4055229d2db282a3451c0a6bd01de
easantanders21 <3823@holbertonschool.com>
2022-05-14 09:10:33 -0500

    task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,17 @@
+""" Class DBStorage """
+
+import os
+
+
+class DBStorage:
+    """ Class DBStorage """
+    __engine = None
+    __session = None
+    
+    def __init__(self):
+        conn = MySQLdb.connect(host=os.environ["HBNB_MYSQL_HOST"],
+                               port=3306, user=os.environ["HBNB_MYSQL_USER"],
+                               passwd=os.environ["HBNB_MYSQL_PWD"],
+                               db=os.environ["HBNB_MYSQL_DB"])
+
+        self.__engine = create_engine(conn, pool_pre_ping=True)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
 from models.base_model import BaseModel, Base
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    cities = relationship("City", cascade="all, delete")
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    #cities = relationship("City", cascade="all, delete")
     name = Column(String(128), nullable=False)
+
+    #name = ""
