85b25b3e40a000989b0fe83be034bd1ca7c903eb
easantanders21 <3823@holbertonschool.com>
2022-05-14 22:07:54 -0500

    task 6

--- console.py
+++ console.py
@@ -150,7 +150,7 @@ class HBNBCommand(cmd.Cmd):
             storage.new(new_instance)
             storage.save()
         except:
-            print("** primer exept **")
+            pass
 
     def help_create(self):
         """ Help information for the create method """
