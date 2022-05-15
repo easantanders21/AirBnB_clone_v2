8b59a1387f41406b1ea432557ae7ac3c8b2381ae
diegojojoayandun <3922@holbertonschool.com>
2022-05-15 06:16:28 -0500

    Fix circular imports in db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,16 @@
 """ Class DBStorage """
 
-from os import environ, getenv
+import os
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from sqlalchemy import create_engine
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 
 class DBStorage:
@@ -14,23 +21,17 @@ class DBStorage:
 
     def __init__(self):
 
-        db = 'mysql+mysqldb://{}:{}@{}/{}'.format(environ["HBNB_MYSQL_USER"],
-                                                  environ["HBNB_MYSQL_PWD"],
-                                                  environ["HBNB_MYSQL_HOST"],
-                                                  environ["HBNB_MYSQL_DB"])
+        db = 'mysql+mysqldb://{}:{}@{}/{}'.format(os.environ["HBNB_MYSQL_USER"],
+                                                  os.environ["HBNB_MYSQL_PWD"],
+                                                  os.environ["HBNB_MYSQL_HOST"],
+                                                  os.environ["HBNB_MYSQL_DB"])
 
         self.__engine = create_engine(db, pool_pre_ping=True)
 
-        if getenv("HBNB_ENV") == "test":
+        if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
         dic_cls = {}
 
         if cls is None:
@@ -42,17 +43,31 @@ class DBStorage:
             query = self.__session.query(cls).all()
 
         for _obj in query:
-            k = '{}.{}'.format(_obj.__class__.__name__, _obj.id)
+            k = "{}.{}".format(_obj.__class__.__name__, _obj.id)
             dic_cls[k] = _obj
 
         return dic_cls
+
+        # for _classes in HBNBCommand.classes:
+        #     if cls is HBNBCommand.classes or:
+        #     res_query = self.__session.query(HBNBCommand.classes[cls].all())
+        #     for obj in res_query:
+        #         k = obj.__class__.__name__ + '.' + obj.id
+        #         k = "{}.{}".format(obj.__class__.__name__, obj.id)
+        #         dic_cls[k] = obj
+        # return
+
+
+
+
+        #print(self.__session.query(cls).all())
 
     def new(self, obj):
         """add the object to the current database session (self.__session)"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes """
+        """commit all changes of the current database session (self.__session)"""
         self.__session.commit()
 
     def delete(self, obj=None):
